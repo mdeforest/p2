88c89e4215bbd848a30cfe807155fcd1e63fbb5b
Michaela DeForest <mdeforest@galileosuite.com>
2018-09-25 15:38:15 -0400

    Create basic structure

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="">
+    <title>Resume Creator</title>
+</head>
+<body>
+    <h1>Resume Creator</h1>
+    <p>Create a Resume by choosing a template and filling out the required fields below</p>
+
+    <form method='POST' action='create.php'>
+        <input type='submit' value='Create'>
+    </form>
+
+</body>
+</html>
+
+
+
