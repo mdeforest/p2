a7905643142c5c903f82a14435b5422378a007c7
Michaela DeForest <mdeforest@galileosuite.com>
2018-10-09 15:24:25 -0400

    Reformat to match styling guide

--- logic/index-logic.php
+++ logic/index-logic.php
@@ -5,11 +5,11 @@ session_start();
 $form = $_SESSION['form'] ?? [];
 $errors = $_SESSION['errors'] ?? [];
 
-
 /* *
  * Get the error message corresponding to the field
  */
-function getError($pattern, array $errors) {
+function getError($pattern, array $errors)
+{
     $matchArray = preg_grep($pattern, $errors);
     $match = array_shift($matchArray);
 
