5acde53f1bd5dd992e387743bd1bd984cb12c5d5
Michaela DeForest <mdeforest@galileosuite.com>
2018-10-09 15:11:23 -0400

    Add logic for getting error message

---
+++ logic/index-logic.php
@@ -0,0 +1,17 @@
+<?php
+
+session_start();
+
+$form = $_SESSION['form'] ?? [];
+$errors = $_SESSION['errors'] ?? [];
+
+
+/* *
+ * Get the error message corresponding to the field
+ */
+function getError($pattern, array $errors) {
+    $matchArray = preg_grep($pattern, $errors);
+    $match = array_shift($matchArray);
+
+    return $match ?? '';
+}
